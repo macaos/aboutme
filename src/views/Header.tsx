15e8c408e26644e3601023e59f77598276d7ce9b
macaos <macaoshu@gmail.com>
2020-03-20 01:38:31 +0900

    Header component

--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { useHistory } from "react-router-dom";
+import { Cubic, TweenLite } from "gsap";
+import React, { useRef, useState } from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  const history = useHistory();
+  const elThis = useRef(null);
+  const [headerState] = useState("main"); // main, sub
+
+  return (
+    <div className="view-Header">
+      <div
+        className="logo"
+        onMouseOver={() => {
+          TweenLite.to(elThis.current, 0.5, {
+            opacity: 1,
+            y: headerState === "sub" ? -20 : 0,
+            ease: Cubic.easeInOut
+          });
+        }}
+        onMouseOut={() => {
+          if (headerState === "sub") {
+            TweenLite.to(elThis.current, 0.5, {
+              opacity: 0.1,
+              y: -27,
+              ease: Cubic.easeInOut
+            });
+          }
+        }}
+        onClick={() => {
+          history.push("/");
+        }}
+        ref={elThis}
+      >
+        <div className="name name1">MA</div>
+        <div className="name name2">
+          <div className="line"></div>
+        </div>
+        <div className="name name3">CAO</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
