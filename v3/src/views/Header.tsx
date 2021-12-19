f4e83ebddf4bcc754a136e4a08ef858eb4379ecf
macaos <help@kareui.com>
2021-12-19 23:18:49 +0900

    Modify Design

--- v3/src/views/Header.tsx
+++ v3/src/views/Header.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 let yOffset;
-let $logo: any
+let $logo: any;
+
+
+
 window.addEventListener('scroll', () => {
+
     yOffset = window.pageYOffset;
     if (!$logo) {
+
         $logo = document.querySelector('.logo');
     }
     if (yOffset < 120) {
         const ratio1 = 1 - yOffset / 120;
         $logo.style.opacity = ratio1
-        const ratio2 = 1 - yOffset / 90;
-
+        // const ratio2 = 1 - yOffset / 90;
     } else {
         $logo.style.opacity = 0;
-
     }
 });
-
 const Header = () => {
     const navigate = useNavigate();
+    $logo = null;
+
     return (
         <header className="header">
             <h1 className="logo" onClick={() => {
