cdded1c78e76dfe50d2eddea4a452298dc371a6b
macaos <51513686+macaos@users.noreply.github.com>
2022-06-18 17:47:36 +0900

    Add Markdown Sass

--- v4/next.config.js
+++ v4/next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
+
+const nextConfig = withMDX({
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
-}
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
+  webpack: config => {
+    config.module.rules.push(
+      {
+        test: /\.md$/,
+        use: "raw-loader"
+      }
+    );
+    return config
+  }
+})
 
 module.exports = nextConfig
+
